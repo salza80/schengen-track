029381997a8799b326b3d8c13fdb8840b128536a
Sally Mclean <smclean17@gmail.com>
2015-04-06 05:50:24 +1000

    Add fields to visits view, model

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,9 +2,19 @@ class Visit < ActiveRecord::Base
   belongs_to :country
   belongs_to :person
   validates :country, :person, :entry_date, presence: true
+  attr_accessor :running_total_days
+
+
 
   def no_days
     return nil if exit_date.nil?
     (exit_date - entry_date).to_i + 1
+  end
+
+  def previous_visits
+    Visit.where("person_id = ? and entry_date <= ? and id <> ?", person_id, entry_date, id)
+  end
+  def post_visits
+    Visit.where("person_id = ? and entry_date >= ? and id <> ?", person_id, entry_date, id)
   end
 end
--- test/models/visit_test.rb
+++ test/models/visit_test.rb
@@ -16,4 +16,16 @@ class VisitTest < ActiveSupport::TestCase
     a =  visits(:two)
     assert_equal nil, a.no_days
   end
+  test 'previous_visits scope' do
+    a = visits(:two)
+    b = a.previous_visits
+    assert_equal 1, b.count
+    assert_equal '2014-03-22'.to_date, b.first.entry_date
+  end
+   test 'post_visits scope' do
+    a = visits(:one)
+    b = a.post_visits
+    assert_equal 1, b.count
+    assert_equal '2014-03-27'.to_date, b.first.entry_date
+  end
 end

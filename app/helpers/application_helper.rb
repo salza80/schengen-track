2df7d26f09eea3424d853525a9c909f35e472ef0
Sally Mclean <smclean17@gmail.com>
2015-08-30 21:25:23 +0200

    highlight selected tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title.titleize }
   end
+
+  def active_if(options)
+    'active' if params.merge(options) == params
+  end
 end

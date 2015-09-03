5d87b167c293c68d9f466c8770e42fd50600c63e
Sally Mclean <smclean17@gmail.com>
2015-09-03 19:10:38 +0200

    remove unused files

--- app/assets/javascripts/visits.js
+++
@@ -1,5 +0,0 @@
-
-// $(document).on("ready page:load", function() {
-//   var CountriesList = new App.Cascade($("#Continent"), $("#visit_country_id"), country, "id", "name", "continent_id")
-// });
-

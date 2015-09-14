0dabf19ac5df522b3b29eb20efe0254d3709d65c
Sally Mclean <smclean17@gmail.com>
2015-09-14 17:50:47 +0200

    styling and root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'visits#index'
+  root 'about#about'
 
   resources :visits
   resources :visas,  except: [:index, :show]

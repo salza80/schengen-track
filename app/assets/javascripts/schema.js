c7f421d99af3ce32eb22d33a26b0676a64c887cd
Sally Mclean <smclean17@gmail.com>
2015-10-09 22:18:37 +0200

    move back and fix

--- app/assets/javascripts/schema.js
+++
@@ -1,6 +0,0 @@
-  {
-    "@context": "http://schema.org",
-    "@type": "Organization",
-    "url": "http://www.schengen-calculator.com",
-    "logo": "http://www.schengen-calculator.com/med.png"
-  }

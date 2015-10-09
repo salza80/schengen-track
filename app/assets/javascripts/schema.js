8b5662470b0fac2c082a2a5ad360adc15f24fbe6
Sally Mclean <smclean17@gmail.com>
2015-10-09 22:11:17 +0200

    move schema data

---
+++ app/assets/javascripts/schema.js
@@ -0,0 +1,6 @@
+  {
+    "@context": "http://schema.org",
+    "@type": "Organization",
+    "url": "http://www.schengen-calculator.com",
+    "logo": "http://www.schengen-calculator.com/med.png"
+  }

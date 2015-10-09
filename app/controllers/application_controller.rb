fc1ee160257f5ebf1977446f1e22091dc5a31d8a
Sally Mclean <smclean17@gmail.com>
2015-10-09 21:41:51 +0200

    fix for new session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,8 @@ class ApplicationController < ActionController::Base
   def default_guest_country
     # country = request.location.data[:country_name]
     # c = Country.find_by(name: country)
-    c || Country.find_by(country_code: 'US')
+    # c || Country.find_by(country_code: 'US')
+    Country.find_by(country_code: 'US')
 
   end
 

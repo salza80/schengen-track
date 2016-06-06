f674f8f1a3d196dde340cd3a1f7ed4b3c2ded041
Sally Mclean <smclean17@gmail.com>
2016-06-06 23:44:40 +0200

    actionmailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'schengen-track.herokuapp.com/' }
 
-  
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',

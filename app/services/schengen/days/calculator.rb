51af1c961c4a4431f0feac1f9bf5914107a55037
Sally Mclean <smclean17@gmail.com>
2016-06-12 16:09:41 +0200

    calculate running 90 in 180 days

--- app/services/schengen/days/calculator.rb
+++ app/services/schengen/days/calculator.rb
@@ -8,14 +8,8 @@ module Schengen
         @person = person
         @visits = @person.visits.to_a
         @calculated_days=[]
-        calculate
-      end
-
-      def calculate
-        return unless @person
-        return if @visits.empty?
-
         generate_days
+        calc_schengen_days
       end
 
       def find_visit(id)
@@ -25,17 +19,23 @@ module Schengen
         nil
       end
 
+      def find_by_date(date)
+        @calculated_days.each do |day|
+          return day if day.the_date == date
+        end
+      end
+
       def generate_days
+        return unless @person
+        @calculated_days = []
         return if @visits.empty?
         begin_date = @visits.first.entry_date
-        end_date = @visits.last.exit_date
+        end_date = @visits.last.exit_date + 180.days
         v = 0
-        return if end_date - begin_date > 500
+        # return if end_date - begin_date > 500
         begin_date.upto(end_date) do | date |
-          puts date
           visit =  @visits[v]
           sd = SchengenDay.new(date)
-
           unless visit.nil?
             set_country(sd,visit)
             if date == visit.exit_date
@@ -46,13 +46,24 @@ module Schengen
           end
           @calculated_days<<sd
         end
+      end
 
+      def calc_schengen_days
+        schengen_days_in_last_180 = 0
 
+        @calculated_days.each_index  do | i |
+          schengen_days_in_last_180 += @calculated_days[i].schengen_day_int
+          if i > 179
+            schengen_days_in_last_180 -= @calculated_days[i-179].schengen_day_int
+          end
+          @calculated_days[i].schengen_days_count = schengen_days_in_last_180
 
+          # puts @calculated_days[i].the_date.to_s + " " + @calculated_days[i].schengen_days_count.to_s
+        end
       end
 
       def total_schengen_days
-        calculated_days.reduce(0){|sum,sd| sum += sd.scheng_num  }
+        calculated_days.reduce(0){|sum,sd| sum += sd.schengen_day_int   }
       end
 
       def calculated_days
@@ -77,10 +88,6 @@ module Schengen
            schengen_day.stayed_country = visit.country
         end
       end
-
-
-      
-
     end
 
     class SchengenDay
@@ -95,15 +102,13 @@ module Schengen
 
         end
 
-        def is_schengen_day
+        def schengen_day?
           (entered_country && entered_country.schengen?) || (stayed_country && stayed_country.schengen?) || (exited_country && exited_country.schengen?)
         end
 
-        def scheng_num
-          return 1 if is_schengen_day
-          0
+        def schengen_day_int 
+          schengen_day? ? 1 : 0
         end
-
 
     end
   end
--- test/services/schengen/days/calculator_test.rb
+++ test/services/schengen/days/calculator_test.rb
@@ -13,4 +13,26 @@ class CalculatorTest < ActiveSupport::TestCase
     assert_equal 102, as.total_schengen_days
  
   end
+
+  test 'schengen day count by day' do
+    person = people(:Test1)
+    as =  Schengen::Days::Calculator.new(person)
+
+    assert_equal 60, as.calculated_days[59].schengen_days_count
+    assert_equal 60, as.calculated_days[60].schengen_days_count
+
+    assert_equal 92, as.find_by_date(Date.new(2014,5,2)).schengen_days_count
+
+    # assert_equal 60, a.schengen_days
+
+    # a = as.find_visit(visits(:testvisit2).id)
+    # assert_equal 60, a.schengen_days
+
+    # a = as.find_visit(visits(:testvisit3).id)
+    # assert_equal 90, a.schengen_days
+    # assert_equal 0, a.schengen_overstay_days
+    # a = as.find_visit(visits(:testvisit4).id)
+    # assert_equal 92, a.schengen_days
+    
+  end
 end

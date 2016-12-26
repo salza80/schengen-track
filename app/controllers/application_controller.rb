d497cccf994525ac1be83fa720ed22095ecb17cd
Sally Mclean <smclean17@gmail.com>
2016-12-26 21:33:10 +1100

    add user-id for google analytics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_person, :amazon
+  helper_method :current_person, :current_user_or_guest_user, :amazon
 
   def current_person
     cp = current_user_or_guest_user.people.first

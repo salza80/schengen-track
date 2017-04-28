1d30ebbc09ef7bca13f6bae96d63adc27db689f2
Sally Mclean <smclean17@gmail.com>
2017-04-28 18:01:47 +0200

    fix facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,7 +258,10 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
 
-  config.omniauth :facebook, ENV['FACEBOOK_ID'] , ENV['FACEBOOK_SECRET'] 
+  config.omniauth :facebook,
+    ENV['FACEBOOK_ID'],
+    ENV['FACEBOOK_SECRET'],
+    token_params: { parse: :json }
   
 
 end

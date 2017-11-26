e6d84389b8ef2dbc9383ec1adad74f23053605ec
Sally Mclean <smclean17@gmail.com>
2017-11-26 20:38:49 +0100

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => Rails.application.sendgrid_username,
+    :user_name      => Rails.application.secrets.sendgrid_username,
     :password       => Rails.application.secrets.sendgrid_password,
     :domain         => 'schengen-calculator.com',
     :enable_starttls_auto => true

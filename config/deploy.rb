dafa2e558f6165f88b0e292da546eabfd530613c
Sally Mclean <smclean17@gmail.com>
2018-02-02 17:05:50 +0100

    puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :repo_url, "git@github.com:salza80/schengen-track.git"
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 set :user,            'deploy'
-set :puma_threads,    [4, 16]
+set :puma_threads,    [2, 5]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing

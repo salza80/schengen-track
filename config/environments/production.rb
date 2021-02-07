bc38859803f7ed74347ea69797f1f133e210a32e
Sally Mclean <smclean17@gmail.com>
2021-02-07 22:59:11 +0100

    force ssl for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -47,6 +49,8 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
+
+  config.force_ssl=true
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]

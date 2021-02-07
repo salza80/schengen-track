7e4acae4a2f8286ceca696b407e1d3a1f23379ef
Sally Mclean <smclean17@gmail.com>
2021-02-07 23:02:11 +0100

    remove force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,8 +50,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  config.force_ssl=true
-
+ 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 

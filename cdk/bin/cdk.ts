a7c0d12805c45b3b8e55501f50b6646baeb96467
Sally Mclean <smclean17@gmail.com>
2023-05-22 22:02:26 +0200

    remove default

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
@@ -5,7 +6,7 @@ import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
 const app = new cdk.App();
 
 new CdkPipelineStack(app, 'CdkPipelineStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+ // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
 
 app.synth();

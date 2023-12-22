4009108bbd30afd7aff1d115ff856ef7cc1a9277
Sally Mclean <smclean17@gmail.com>
2023-12-22 18:01:38 +0100

    fi

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
     puts cloudfront_domain
     if cloudfront_domain.present?
       full_url = "https://#{cloudfront_domain}#{path}"
-      redirect_to full_url, *optional_params
+      redirect_to full_url, allow_other_host: true, *optional_params
     else
       redirect_to path, *optional_params
     end

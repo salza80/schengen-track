e56d0871611ff096eee91b439ae0d02d80d3fe28
Sally Mclean <smclean17@gmail.com>
2023-12-22 14:56:31 +0100

    redirect

--- src/app/controllers/about_controller.rb
+++ src/app/controllers/about_controller.rb
@@ -7,7 +7,6 @@ class AboutController < ApplicationController
     return if params[:nationality].nil?
     @country = Country.find_by_nationality(params[:nationality])
                .outside_schengen.first
-    expires_in 1.day, public: true
     fail ActionController::RoutingError, 'Page Not Found' if @country.nil?
   end
 

d01ad1840018856913efe9032a7417458899dd7d
Sally Mclean <smclean17@gmail.com>
2023-12-23 23:11:07 +0100

    add

--- src/app/controllers/about_controller.rb
+++ src/app/controllers/about_controller.rb
@@ -3,15 +3,14 @@ class AboutController < ApplicationController
   # GET /about/
   # GET /about/:nationaity
   def about
+    if current_user_or_guest_user.is_guest?
+      response.headers['Cache-Control'] = 'public, max-age=3600'
+    end
     @country = nil
     return if params[:nationality].nil?
     @country = Country.find_by_nationality(params[:nationality])
                .outside_schengen.first
     fail ActionController::RoutingError, 'Page Not Found' if @country.nil?
-
-    if current_user_or_guest_user.is_guest?
-      response.headers['Cache-Control'] = 'public, max-age=3600'
-    end
   end
 
   # GET /disclaimer/

1bc80404834666b508ecaffaa766180c2141fc37
Sally Mclean <smclean17@gmail.com>
2024-01-14 04:37:14 +0100

    fix

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -46,10 +46,6 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-<<<<<<< HEAD:src/config/environments/production.rb
-
-=======
->>>>>>> staging:config/environments/production.rb
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true

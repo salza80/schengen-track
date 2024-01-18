f2bda495b35ebf63f01b3e751019a53b4f665864
Sally Mclean <smclean17@gmail.com>
2024-01-18 01:20:33 +0100

    staging and prod deploys (#65)
    * env deploys
    
    * deploys
    
    * specify stack
    
    * fix
    
    * fix
    
    * fix param path
    
    * fix param path
    
    * cf
    
    * testprod
    
    * test
    
    * test
    
    * readme

--- deployment/lib/rails-lambda-stack.ts
+++ deployment/lib/rails-lambda-stack.ts
@@ -3,11 +3,14 @@ import { Construct } from 'constructs';
 import { HttpApiConstruct } from './http-api';
 
 export interface RailsLambdaStackProps extends cdk.StackProps {
+  domain: string,
+  sslArn: string,
+  paramPath: string
 }
 
 export class RailsLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: RailsLambdaStackProps) {
     super(scope, id, props);
-    new HttpApiConstruct(this, 'Http', {});
+    new HttpApiConstruct(this, 'Http', { domain: props.domain, sslArn: props.sslArn, paramPath: props.paramPath});
   }
 }

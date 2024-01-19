2059337ca01efab94d3f8fb89a4c1c3a6bd7aeb2
Sally Mclean <smclean17@gmail.com>
2024-01-19 16:40:58 +0100

    set default_url_options to fix email links (#70)

--- src/config/application.rb
+++ src/config/application.rb
@@ -10,6 +10,7 @@ module SchengenTrack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    Rails.application.routes.default_url_options[:host] = "localhost:3000"
 
     # Configuration for the application, engines, and railties goes here.
     #

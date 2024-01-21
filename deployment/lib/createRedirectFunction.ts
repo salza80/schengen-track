b70ceb40d4ddbf4ad2d1ba6fe7f6434d9d12c2cd
Sally Mclean <smclean17@gmail.com>
2024-01-21 14:57:00 +0100

    fix redirectfn

--- deployment/lib/createRedirectFunction.ts
+++ deployment/lib/createRedirectFunction.ts
@@ -1,28 +1,24 @@
 // a couldfront function to rewrite from altDomain to targetDomain
 export function createRedirectFunction(altDomain: string, targetDomain: string): string {
-    return `
-        function handler(event) {
-            const request = event.request;
-            const headers = request.headers;
+  return `
+    function handler(event) {
+      var request = event.request;
+      var headers = request.headers;
+      var host = request.headers.host.value;
 
-            // Check if the request is coming from altDomain
-            if (headers.host && headers.host[0].value === '${altDomain}') {
-                // Redirect to targetDomain
-                return {
-                    status: '301',
-                    statusDescription: 'Moved Permanently',
-                    headers: {
-                        location: [{
-                            key: 'Location',
-                            value: 'https://${targetDomain}' + request.uri,
-                        }],
-                    },
-                };
-            }
+      // Check if the request is coming from altDomain
+      if (host === '${altDomain}') {
+        // Redirect to targetDomain
+        return {
+          status: 302,
+          statusDescription: 'Found',
+          headers: 
+          { "location": { "value": 'https://${targetDomain}' }}
+        };
+      }
 
-            // Continue with the request as is
-            return request;
-        }
-    `;
+      // Continue with the request as is
+      return request;
+    }
+  `;
 }
-

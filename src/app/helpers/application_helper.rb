b66791c015ad805ea0e71653ff04ad41ed6f0cde
Sally Mclean <smclean17@gmail.com>
2024-04-20 20:02:43 +0200

    german translations

--- src/app/helpers/application_helper.rb
+++ src/app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
   def active_if(options)
     'active' if params.merge(options) == params
   end
+
+  def language_selector
+    locales = Rails.application.config.i18n.available_locales
+    current_locale = I18n.locale.to_sym
+  
+    content_tag(:span) do
+      locales.map do |locale|
+        content_tag(:span, class: locale == current_locale ? 'active' : '') do
+          link_to(locale.upcase, locale: locale)
+        end
+      end.join(" | ").html_safe
+    end
+  end
 end

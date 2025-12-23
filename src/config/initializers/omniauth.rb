7d37057bf64027dc70c9b9ed76ca2c597462d944
Sally Mclean <smclean17@gmail.com>
2025-12-23 12:28:20 +1100

    test

--- src/config/initializers/omniauth.rb
+++ src/config/initializers/omniauth.rb
@@ -4,3 +4,7 @@
 # GET requests are standard for OAuth and work better on mobile browsers
 # The OAuth 2.0 'state' parameter provides built-in CSRF protection
 OmniAuth.config.allowed_request_methods = %i[get post]
+
+# Silence the GET warning since we've consciously chosen to use GET for OAuth
+# and are relying on OAuth 2.0's built-in state parameter for CSRF protection
+OmniAuth.config.silence_get_warning = true

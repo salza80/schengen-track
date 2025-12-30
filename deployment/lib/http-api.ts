bee8ddae3e60c8e67f36000bc704bf71e832b795
Sally Mclean <smclean17@gmail.com>
2025-12-30 23:27:31 +1100

    chore: run tasks in own lambda not via api (#132)
    * js updates
    
    * lambda tssks
    
    * fix
    
    * migrate in deploy job
    
    * run
    
    * fix
    
    * remove guest check
    
    * update readmes
    
    * increas timeout

--- deployment/lib/http-api.ts
+++ deployment/lib/http-api.ts
@@ -33,16 +33,27 @@ export class HttpApiConstruct extends Construct {
     super(scope, id);
 
     // Rails HTTP API container image with AWS Lambda Ruby Runtime Interface Client
-    const apiContainerImage = lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../src'), {
+    const imageAssetPath = path.join(__dirname, '../../src');
+    const baseImageProps = {
       //platform: Platform.LINUX_ARM64,
       platform: Platform.LINUX_AMD64,
       ignoreMode: cdk.IgnoreMode.DOCKER,
-
       entrypoint: [
         '/usr/local/bundle/bin/aws_lambda_ric',
       ],
+    };
+
+    const apiContainerImage = lambda.DockerImageCode.fromImageAsset(imageAssetPath, {
+      ...baseImageProps,
       cmd: [
         'lambda_http.handler',
+      ],
+    });
+
+    const opsContainerImage = lambda.DockerImageCode.fromImageAsset(imageAssetPath, {
+      ...baseImageProps,
+      cmd: [
+        'lambda_tasks.handler',
       ],
     });
   
@@ -83,6 +94,15 @@ export class HttpApiConstruct extends Construct {
       environment: apiContainerEnvironment,
 
       timeout: cdk.Duration.minutes(1),
+      tracing: lambda.Tracing.ACTIVE,
+    });
+
+    const opsFunction = new lambda.DockerImageFunction(this, 'OpsFunction', {
+      architecture: lambda.Architecture.X86_64,
+      memorySize: 2048,
+      code: opsContainerImage,
+      environment: apiContainerEnvironment,
+      timeout: cdk.Duration.minutes(15),
       tracing: lambda.Tracing.ACTIVE,
     });
 
@@ -252,8 +272,14 @@ export class HttpApiConstruct extends Construct {
       }
     });
 
-    new cdk.CfnOutput(this, 'CloudFrontUrl', {
+    const cloudFrontUrlOutput = new cdk.CfnOutput(this, 'CloudFrontUrl', {
       value: cloudfrontDist.domainName!,
     });
+    cloudFrontUrlOutput.overrideLogicalId('CloudFrontUrl');
+
+    const opsLambdaNameOutput = new cdk.CfnOutput(this, 'OpsLambdaFunctionName', {
+      value: opsFunction.functionName,
+    });
+    opsLambdaNameOutput.overrideLogicalId('OpsLambdaFunctionName');
   }
 }
---
+++ src/lambda_tasks.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'json'
+require_relative './config/environment'
+require_relative './lib/lambda/task_executor'
+
+module LambdaTasks
+  module_function
+
+  def handler(event:, context:)
+    result = Lambda::TaskExecutor.execute(event || {})
+    { statusCode: 200, body: JSON.generate(result) }
+  rescue => e
+    error_response = { success: false, error: e.message }
+    { statusCode: 500, body: JSON.generate(error_response) }
+  end
+end
+
+# Shim for AWS Lambda handler lookup
+def handler(event:, context:)
+  LambdaTasks.handler(event: event, context: context)
+end

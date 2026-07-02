28db4b2ac34ac677d83d7c73f34b53daaa4277fe
Sally Mclean <smclean17@gmail.com>
2026-07-02 21:24:17 +0200

    chore(deps): update ruby gems (#175)
    * gem security audit auto
    
    * bump puma
    
    * fix
    
    * update

---
+++ src/app/assets/config/manifest.js
@@ -0,0 +1,8 @@
+//= link_tree ../images
+//= link application.js
+//= link application.css
+//= link core.js
+//= link calendar_bundle.js
+//= link visits_bundle.js
+//= link calendar.css
+//= link blog.css
